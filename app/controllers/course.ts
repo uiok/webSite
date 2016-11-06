db30c0644dacaed055717da981869cb582924006
samtw <sam@infocloud.com.tw>
2016-11-06 23:29:14 +0800

    add_view

---
+++ app/controllers/course.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector:"coursrList",
+	templateUrl:"./app/views/course/list.html"	
+})
+
+export class CourseComponet{
+	
+}
---
+++ app/controllers/person.ts
@@ -0,0 +1,47 @@
+import { Component } from  '@angular/core';
+
+@Component({
+	selector:"personBasic",
+	templateUrl:"./app/views/person/basic.html"
+})
+
+export class BasicComponent {
+	
+} 
+
+
+@Component({
+	selector:"certificate",
+	templateUrl:"./app/views/person/certificateRecord.html"
+
+})
+export class CertificateComponent {
+	
+} 
+
+@Component({
+	selector:"personList",
+	templateUrl:"./app/views/person/List.html"
+
+})
+export class PersonListComponent {
+	
+} 
+
+@Component({
+	selector:"record",
+	templateUrl:"./app/views/person/record.html"
+
+})
+export class RecordComponent {
+	
+} 
+
+@Component({
+	selector:"testRecord",
+	templateUrl:"./app/views/person/testRecord.html"
+
+})
+export class TestRecordComponent {
+	
+} 
